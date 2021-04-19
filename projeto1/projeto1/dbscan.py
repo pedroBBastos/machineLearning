c4de648aa75b1ee877f83be108327600bde708cb
Pedro Bastos <pedro.bastos@unisoma.com>
2021-04-19 10:52:42 -0300

    Refactor código

--- projeto1/projeto1/dbscan.py
+++ projeto1/projeto1/dbscan.py
@@ -1,5 +1,94 @@
 import numpy as np
-import sys
+import scipy as scipy
+import matplotlib.pyplot as plt
+
+from sklearn import preprocessing
+import sklearn.metrics as sklMetrics
+
+import elbow as em
+import scipy.spatial
+
+
+def executeDBSCAN(dataTraining, dataTest, eps_values, minPts):
+    pontosTreino, _ = dataTraining.shape
+
+    min_max_scaler = preprocessing.MinMaxScaler()
+    dataTraining = min_max_scaler.fit_transform(dataTraining)
+
+    matrixDeDistancia = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(dataTraining))
+
+    # knee gráfico
+    thirdMinDists = np.sort(np.partition(matrixDeDistancia, 4)[:, 4])
+    em.plot_elbow_graphic(thirdMinDists, range(0, len(thirdMinDists)), 2)
+
+    clusteredPorEps = []
+    corePointsPorEps = []
+    clusterNumbersPorEps = []
+
+    for eps in eps_values:
+        clustered, corePointsList = DBSCAN(dataTraining, matrixDeDistancia, eps, minPts)
+        clusterNumbers = np.unique(clustered[:, 3])
+
+        clusteredPorEps.append(clustered)
+        corePointsPorEps.append(corePointsList)
+        clusterNumbersPorEps.append(clusterNumbers)
+
+        # silhouette_score como métrica para avaliar
+        # silhouette dá erro se for executado com apenas um cluster
+        if np.unique(clustered[:, 3]).shape[0] != 1:
+            silhouette_score = sklMetrics.silhouette_score(dataTraining, clustered[:, 3])
+
+        for ci in clusterNumbers:
+            ci = clustered[clustered[:, 3] == ci]
+            ci = ci[:, :2]
+            cix = ci[:, 0]
+            ciy = ci[:, 1]
+            plt.plot(cix, ciy, color=np.random.random(3), marker='x', linestyle='')
+        plt.show()
+
+    ###########################################################
+    # Test
+    ###########################################################
+
+    dataTest = min_max_scaler.fit_transform(dataTest)
+    pontosTeste, _ = dataTest.shape
+
+    chosenClustered = clusteredPorEps[0]
+    chosenClustered = np.delete(chosenClustered, 2, 1)
+    initCluster = -1 * np.ones((pontosTeste, 1))  # -1 (no cluster)
+    dataTest = np.hstack((dataTest, initCluster))
+
+    newMatrix = np.concatenate((chosenClustered[:, :2], dataTest[:, :2]))
+    novaMatrixDeDistancia = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(newMatrix))
+    novaMatrixDeDistancia = novaMatrixDeDistancia[516:, :516]
+    print(novaMatrixDeDistancia)
+
+    indicesPontosMaisProximos = np.argmin(novaMatrixDeDistancia, axis=1)
+    mask = np.zeros((pontosTeste, pontosTreino), dtype=bool) # TODO trocar para usar os shapes
+    mask[np.arange(len(mask)), indicesPontosMaisProximos] = True
+    menoresDistancias = novaMatrixDeDistancia[mask]
+
+    # atribuindo número de cluster aos novos pontos
+    toAssignCluster = np.where(menoresDistancias <= eps_values[0])[0]
+    dataTest[toAssignCluster, 2] = chosenClustered[indicesPontosMaisProximos[toAssignCluster], 2]
+
+    clusterNumbersTeste = clusterNumbersPorEps[0]
+    for ci in clusterNumbersTeste:
+        cor = np.random.random(3)
+
+        pontosCi = chosenClustered[chosenClustered[:, 2] == ci]
+        pontosCi = pontosCi[:, :2]
+        pontosCix = pontosCi[:, 0]
+        pontosCiy = pontosCi[:, 1]
+        plt.plot(pontosCix, pontosCiy, color=cor, marker='x', linestyle='')
+
+        ciTeste = dataTest[dataTest[:, 2] == ci]
+        ciTeste = ciTeste[:, :2]
+        ciTestex = ciTeste[:, 0]
+        ciTestey = ciTeste[:, 1]
+        plt.plot(ciTestex, ciTestey, color=cor, marker='s', linestyle='')
+    plt.show()
+
 
 # Core — A point is a core point if it has more than MinPts points within eps. Border — This is a point that has
 # at least one Core point at a distance n. Noise — This is a point that is neither a Core nor a Border. And it has
@@ -28,7 +117,6 @@ def DBSCAN(data, matrixDeDistancia, eps, minPts):
 def walkThroughPontosDaDensidade(i, matrixDeDistancia,
                                  data, eps, minPts,
                                  clusterNumber, dimensoes, corePointsList):
-
     if data[i][dimensoes] == 0:
         # ponto i visitado
         data[i][dimensoes] = 1
@@ -38,9 +126,28 @@ def walkThroughPontosDaDensidade(i, matrixDeDistancia,
         pontosWithinEps = np.where(distanciasPontoAtual <= eps)[0]
         if pontosWithinEps.shape[0] >= minPts:
             corePointsList.append([i, clusterNumber])
-            data[pontosWithinEps, dimensoes+1] = clusterNumber  # pontos dentro de eps pertencem ao cluster atual
+            data[pontosWithinEps, dimensoes + 1] = clusterNumber  # pontos dentro de eps pertencem ao cluster atual
 
             for p in pontosWithinEps:
                 walkThroughPontosDaDensidade(p, matrixDeDistancia, data,
                                              eps, minPts, clusterNumber,
                                              dimensoes, corePointsList)
+
+# def executeDBSCAN3D(data):
+#     min_max_scaler = preprocessing.MinMaxScaler()
+#     data = min_max_scaler.fit_transform(data)
+#
+#     matrixDeDistancia = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(data))
+#
+#     clustered = dbscan.DBSCAN(data, matrixDeDistancia, 0.13, 11)
+#     clusterNumbers = np.unique(clustered[:, 4])
+#
+#     ax = plt.axes(projection='3d')
+#     for ci in clusterNumbers:
+#         ci = clustered[clustered[:, 4] == ci]
+#         ci = ci[:, :3]
+#         cix = ci[:, 0]
+#         ciy = ci[:, 1]
+#         ciz = ci[:, 2]
+#         ax.scatter(cix, ciy, ciz, color=np.random.random(3))
+#     plt.show()
--- projeto1/projeto1/kMeans.py
+++ projeto1/projeto1/kMeans.py
@@ -1,4 +1,83 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import preprocessing
+
+import kMeans as km
+import elbow as em
+
+
+def executeKMeans(dataTraining, dataTest):
+    min_max_scaler = preprocessing.MinMaxScaler()
+    data = min_max_scaler.fit_transform(dataTraining)
+
+    centroidesPorK = []
+    clusteredPorK = []
+    clusteredNumbersPorK = []
+    k_clusters = [1, 2, 3, 4, 5, 6, 7]
+    elbow_values_plot = []
+
+    for k in k_clusters:
+        clustered, centroides = km.kMeans(data, k)
+        clusterNumbers = np.unique(clustered[:, 2])
+
+        centroidesPorK.append(centroides)
+        clusteredPorK.append(clustered)
+        clusteredNumbersPorK.append(clusterNumbers)
+
+        for ci in clusterNumbers:
+            ci = clustered[clustered[:, 2] == ci]
+            ci = ci[:, :2]
+            cix = ci[:, 0]
+            ciy = ci[:, 1]
+            plt.plot(cix, ciy, color=np.random.random(3), marker='x', linestyle='')
+        plt.show()
+
+        value = em.elbow_value(clustered, centroides)
+        elbow_values_plot.append(value)
+
+    em.plot_elbow_graphic(elbow_values_plot, k_clusters, 1)
+
+    ################################################################
+    # melhor k = 3
+    # verificando para qual cluster pertencem os dados de teste
+    ################################################################
+
+    dataTest = min_max_scaler.fit_transform(dataTest)
+
+    linhas = dataTest.shape[0]
+    colunaCluster = -1 * np.zeros((linhas, 1))
+    dataTest = np.hstack((dataTest, colunaCluster))
+
+    centroidesTeste = centroidesPorK[2]
+    clusteredTeste = clusteredPorK[2]
+    clusterNumbersTeste = clusteredNumbersPorK[2]
+
+    for ponto in dataTest:
+        # obtendo as distancias para os centroides do ponto atual
+        normsFromCentroides = np.linalg.norm(ponto[:2] - centroidesTeste, axis=1)
+        # computando o centroide mais próximo
+        nearestCentroideIndex = np.where(normsFromCentroides == np.amin(normsFromCentroides))[0][0]
+        # indicando no ponto o indice do centroide ao qual ele pertence
+        ponto[2] = nearestCentroideIndex
+
+    # np.append(clusteredTeste, dataTest[:, :2])
+
+    for ci in clusterNumbersTeste:
+        cor = np.random.random(3)
+
+        pontosCi = clusteredTeste[clusteredTeste[:, 2] == ci]
+        pontosCi = pontosCi[:, :2]
+        pontosCix = pontosCi[:, 0]
+        pontosCiy = pontosCi[:, 1]
+        plt.plot(pontosCix, pontosCiy, color=cor, marker='x', linestyle='')
+
+        ciTeste = dataTest[dataTest[:, 2] == ci]
+        ciTeste = ciTeste[:, :2]
+        ciTestex = ciTeste[:, 0]
+        ciTestey = ciTeste[:, 1]
+        plt.plot(ciTestex, ciTestey, color=cor, marker='s', linestyle='')
+    plt.show()
 
 
 def kMeans(data, k):
