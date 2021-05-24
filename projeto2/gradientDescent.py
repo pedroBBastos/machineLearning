7256b9a8159fa8f3fe61d90bf2ff0021fd28ef6d
Pedro Bastos <pedro.bastos@unisoma.com>
2021-05-24 19:11:21 -0300

    Usando classe abstrata para diferenciação de cálculo linear de cálculo polinomial

--- projeto2/gradientDescent.py
+++ projeto2/gradientDescent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math as math
+import abc
 
 
 # inicializa randomicamente os tethas a serem utilizados
@@ -10,19 +11,11 @@ def initializeTethas(nFeatures, featuresRangeArray):
     return tethas
 
 
-# calcula hTetha(xi), sendo xi o vetor com os dados de uma entrada ("linha")
-def calculateHypothesisForPoint(tethas, entry):
-    hyphotesis = tethas[0]
-    for i in range(entry.size):
-        hyphotesis += entry[i] * tethas[i + 1]
-    return hyphotesis
-
-
-def updateTetha(tethas, currentTetha, learningRate, entries, results, m):
+def updateTetha(baseGradientDescent, tethas, currentTetha, learningRate, entries, results, m):
     somatorio = 0
     for i in range(m):
         ithX = 1 if currentTetha == 0 else entries[i][currentTetha - 1]
-        somatorio += (calculateHypothesisForPoint(tethas, entries[i]) - results[i]) * ithX
+        somatorio += (baseGradientDescent.calculateHypothesisForPoint(tethas, entries[i]) - results[i]) * ithX
     return tethas[currentTetha] - learningRate * (somatorio / m)
 
 
@@ -30,16 +23,39 @@ def updateTetha(tethas, currentTetha, learningRate, entries, results, m):
 # entries -> vetor com m entradas de n features
 # results -> vetor com m resultados
 # learningRate -> learning rate
-def runEpoch(tethas, entries, results, learningRate):
+def runEpoch(baseGradientDescent, tethas, entries, results, learningRate):
     m = entries.shape[0]
     updatedTethas = np.zeros(tethas.size)
     for ithTetha in range(tethas.size):
-        updatedTethas[ithTetha] = updateTetha(tethas, ithTetha, learningRate, entries, results, m)
+        updatedTethas[ithTetha] = updateTetha(baseGradientDescent, tethas, ithTetha, learningRate, entries, results, m)
     return updatedTethas
 
 
-def errorFunction(tethas, entries, results, m):
+def errorFunction(baseGradientDescent, tethas, entries, results, m):
     somatorio = 0
     for i in range(m):
-        somatorio += math.pow((calculateHypothesisForPoint(tethas, entries[i]) - results[i]), 2)
+        somatorio += math.pow((baseGradientDescent.calculateHypothesisForPoint(tethas, entries[i]) - results[i]), 2)
     return somatorio / (2 * m)
+
+
+class BaseGradientDescent(metaclass=abc.ABCMeta):
+    # calcula hTetha(xi), sendo xi o vetor com os dados de uma entrada ("linha")
+    @abc.abstractmethod
+    def calculateHypothesisForPoint(self, tethas, entry):
+        pass
+
+
+class LinearRegression(BaseGradientDescent):
+    def calculateHypothesisForPoint(self, tethas, entry):
+        hyphotesis = tethas[0]
+        for i in range(entry.size):
+            hyphotesis += entry[i] * tethas[i + 1]
+        return hyphotesis
+
+
+class PolinomialRegression(BaseGradientDescent):
+    def calculateHypothesisForPoint(self, tethas, entry):
+        hyphotesis = tethas[0]
+        for i in range(entry.size):
+            hyphotesis += tethas[i + 1] * math.pow(entry[i], i + 1)  # ^1, ^2, ^3...
+        return hyphotesis
--- projeto2/main.py
+++ projeto2/main.py
@@ -1,16 +1,31 @@
 import numpy as np
 import gradientDescent as gd
 
-
 if __name__ == '__main__':
     data = np.genfromtxt('kick2.dat')
     entries = data[:, :2]
     results = data[:, 2]
 
+    #################################################################################
+    # treinando modelo linear
+    #################################################################################
+    print("--------------------- treinando modelo linear ----------------------")
     tethas = gd.initializeTethas(2, [(-3.0, 3.0), (0.0, 2.0)])
-
-    print(tethas)
+    linearGD = gd.LinearRegression()
     for i in range(0, 1000):
-        tethas = gd.runEpoch(tethas, entries, results, 0.04)
-        print(gd.errorFunction(tethas, entries, results, entries.shape[0]))
+        tethas = gd.runEpoch(linearGD, tethas, entries, results, 0.04)
+        print(gd.errorFunction(linearGD, tethas, entries, results, entries.shape[0]))
+    print("final tethas -->>> ")
+    print(tethas)
 
+    #################################################################################
+    # treinando modelo polinomial
+    #################################################################################
+    print("--------------------- treinando modelo polinomial ----------------------")
+    tethas = gd.initializeTethas(2, [(-3.0, 3.0), (0.0, 2.0)])
+    polinomialGD = gd.PolinomialRegression()
+    for i in range(0, 1000):
+        tethas = gd.runEpoch(polinomialGD, tethas, entries, results, 0.04)
+        print(gd.errorFunction(polinomialGD, tethas, entries, results, entries.shape[0]))
+    print("final tethas -->>> ")
+    print(tethas)
