3eadaa0647cb35f1e05fd215ebf3c3d40f38451e
Pedro Bastos <pedro.bastos@unisoma.com>
2021-05-26 23:11:51 -0300

    Refazendo regressão linear

--- projeto2/gradientDescent.py
+++ projeto2/gradientDescent.py
@@ -3,59 +3,56 @@ import math as math
 import abc
 
 
-# inicializa randomicamente os tethas a serem utilizados
-def initializeTethas(nFeatures, featuresRangeArray):
-    tethas = np.zeros((nFeatures + 1))
-    for i in range(len(featuresRangeArray)):
-        tethas[i + 1] = np.random.uniform(low=featuresRangeArray[i][0], high=featuresRangeArray[i][1])
-    return tethas
+# inicializa randomicamente os thetas a serem utilizados
+def initializeThetas(qtdThetas, rangeTuple):
+    thetas = np.zeros(qtdThetas)
+    for i in range(qtdThetas):
+        thetas[i] = np.random.uniform(low=rangeTuple[0], high=rangeTuple[1])
+    return thetas
 
 
-def updateTetha(baseGradientDescent, tethas, currentTetha, learningRate, entries, results, m):
+def updatetheta(baseGradientDescent, thetas, currenttheta, learningRate, entries, results, m):
     somatorio = 0
     for i in range(m):
-        ithX = 1 if currentTetha == 0 else entries[i][currentTetha - 1]
-        somatorio += (baseGradientDescent.calculateHypothesisForPoint(tethas, entries[i]) - results[i]) * ithX
-    return tethas[currentTetha] - learningRate * (somatorio / m)
+        ithX = 1 if currenttheta == 0 else entries[i]
+        somatorio += (baseGradientDescent.calculateHypothesisForPoint(thetas, entries[i]) - results[i]) * ithX
+    return thetas[currenttheta] - learningRate * (somatorio / m)
 
 
-# tethas -> vetor de tethas atual (n features + 1)
+# thetas -> vetor de thetas atual (n features + 1)
 # entries -> vetor com m entradas de n features
 # results -> vetor com m resultados
 # learningRate -> learning rate
-def runEpoch(baseGradientDescent, tethas, entries, results, learningRate):
+def runEpoch(baseGradientDescent, thetas, entries, results, learningRate):
     m = entries.shape[0]
-    updatedTethas = np.zeros(tethas.size)
-    for ithTetha in range(tethas.size):
-        updatedTethas[ithTetha] = updateTetha(baseGradientDescent, tethas, ithTetha, learningRate, entries, results, m)
-    return updatedTethas
+    updatedthetas = np.zeros(thetas.size)
+    for iththeta in range(thetas.size):
+        updatedthetas[iththeta] = updatetheta(baseGradientDescent, thetas, iththeta, learningRate, entries, results, m)
+    return updatedthetas
 
 
-def errorFunction(baseGradientDescent, tethas, entries, results, m):
+def errorFunction(baseGradientDescent, thetas, entries, results, m):
     somatorio = 0
     for i in range(m):
-        somatorio += math.pow((baseGradientDescent.calculateHypothesisForPoint(tethas, entries[i]) - results[i]), 2)
+        somatorio += math.pow((baseGradientDescent.calculateHypothesisForPoint(thetas, entries[i]) - results[i]), 2)
     return somatorio / (2 * m)
 
 
 class BaseGradientDescent(metaclass=abc.ABCMeta):
-    # calcula hTetha(xi), sendo xi o vetor com os dados de uma entrada ("linha")
+    # calcula htheta(xi), sendo xi o vetor com os dados de uma entrada ("linha")
     @abc.abstractmethod
-    def calculateHypothesisForPoint(self, tethas, entry):
+    def calculateHypothesisForPoint(self, thetas, entry):
         pass
 
 
 class LinearRegression(BaseGradientDescent):
-    def calculateHypothesisForPoint(self, tethas, entry):
-        hyphotesis = tethas[0]
-        for i in range(entry.size):
-            hyphotesis += entry[i] * tethas[i + 1]
-        return hyphotesis
+    def calculateHypothesisForPoint(self, thetas, entry):
+        return thetas[0] + entry * thetas[1]
 
 
 class PolinomialRegression(BaseGradientDescent):
-    def calculateHypothesisForPoint(self, tethas, entry):
-        hyphotesis = tethas[0]
+    def calculateHypothesisForPoint(self, thetas, entry):
+        hyphotesis = thetas[0]
         for i in range(entry.size):
-            hyphotesis += tethas[i + 1] * math.pow(entry[i], i + 1)  # ^1, ^2, ^3...
+            hyphotesis += thetas[i + 1] * math.pow(entry[i], i + 1)  # ^1, ^2, ^3...
         return hyphotesis
--- projeto2/main.py
+++ projeto2/main.py
@@ -3,16 +3,46 @@ import gradientDescent as gd
 import matplotlib.pyplot as plt
 
 
-def plotLinear(dataXY, dataZ, tethas):
+# def plotPolinomial(dataXY, dataZ, thetas):
+#     ax = plt.axes(projection='3d')
+#     dataX = dataXY[:, 0]
+#     dataY = dataXY[:, 1]
+#     ax.scatter(dataX, dataY, dataZ, color=np.random.random(3))
+#
+#     plt.xlim([-3.0, 3.0])
+#     plt.ylim([0.0, 2.0])
+#
+#     x = np.linspace(-3, 3, 100)
+#     y = np.linspace(0, 2, 100)
+#     finalFunction = thetas[0] + x * thetas[1] + y ** 2 * thetas[2]
+#     ax.plot(x, y, finalFunction)
+#     ax.set_zlim((0.0, 0.3))
+#     plt.show()
+#
+#
+# def runPolinomial(entries, results):
+#     print("--------------------- treinando modelo polinomial ----------------------")
+#     thetas = gd.initializethetas(2, [(-3.0, 3.0), (0.0, 2.0)])
+#     polinomialGD = gd.PolinomialRegression()
+#     for i in range(0, 2000):
+#         thetas = gd.runEpoch(polinomialGD, thetas, entries, results, 0.2)
+#         print(gd.errorFunction(polinomialGD, thetas, entries, results, entries.shape[0]))
+#     print("final thetas -->>> ")
+#     print(thetas)
+#     plotPolinomial(entries, results, thetas)
+
+def plotLinear(coordenatesArray, thetaX, thetaY, thetaZ):
     ax = plt.axes(projection='3d')
-    dataX = dataXY[:, 0]
-    dataY = dataXY[:, 1]
+    dataX = coordenatesArray[:, 0]
+    dataY = coordenatesArray[:, 1]
+    dataZ = coordenatesArray[:, 2]
     ax.scatter(dataX, dataY, dataZ, color=np.random.random(3))
 
-    x = np.linspace(-3, 3, 100)
-    y = np.linspace(0, 2, 100)
-    finalFunction = tethas[0] + x * tethas[1] + y * tethas[2]
-    ax.plot(x, y, finalFunction)
+    tempo = np.linspace(1/60, 1, 60)
+    x = thetaX[0] + tempo * thetaX[1]
+    y = thetaY[0] + tempo * thetaY[1]
+    z = thetaZ[0] + tempo * thetaZ[1]
+    ax.plot(x, y, z)
 
     ax.set_xlim(-3.0, 3.0)
     ax.set_ylim(0.0, 2.0)
@@ -21,48 +51,31 @@ def plotLinear(dataXY, dataZ, tethas):
     plt.show()
 
 
-def plotPolinomial(dataXY, dataZ, tethas):
-    ax = plt.axes(projection='3d')
-    dataX = dataXY[:, 0]
-    dataY = dataXY[:, 1]
-    ax.scatter(dataX, dataY, dataZ, color=np.random.random(3))
-
-    plt.xlim([-3.0, 3.0])
-    plt.ylim([0.0, 2.0])
-
-    x = np.linspace(-3, 3, 100)
-    y = np.linspace(0, 2, 100)
-    finalFunction = tethas[0] + x * tethas[1] + y ** 2 * tethas[2]
-    ax.plot(x, y, finalFunction)
-    ax.set_zlim((0.0, 0.3))
-    plt.show()
-
-
-def runPolinomial(entries, results):
-    print("--------------------- treinando modelo polinomial ----------------------")
-    tethas = gd.initializeTethas(2, [(-3.0, 3.0), (0.0, 2.0)])
-    polinomialGD = gd.PolinomialRegression()
-    for i in range(0, 2000):
-        tethas = gd.runEpoch(polinomialGD, tethas, entries, results, 0.2)
-        print(gd.errorFunction(polinomialGD, tethas, entries, results, entries.shape[0]))
-    print("final tethas -->>> ")
-    print(tethas)
-    plotPolinomial(entries, results, tethas)
-
-
-def runLinear(entries, results):
+def runLinear(coordenatesArray, time):
     print("--------------------- treinando modelo linear ----------------------")
-    tethas = gd.initializeTethas(2, [(-3.0, 3.0), (0.0, 2.0)])
+    thetaX = gd.initializeThetas(2, (-3.0, 3.0))
+    thetaY = gd.initializeThetas(2, (0.0, 2.0))
+    thetaZ = gd.initializeThetas(2, (0.0, 0.6))
+
     linearGD = gd.LinearRegression()
+
+    dataX = coordenatesArray[:, 0]
+    dataY = coordenatesArray[:, 1]
+    dataZ = coordenatesArray[:, 2]
+
     for i in range(0, 2000):
-        tethas = gd.runEpoch(linearGD, tethas, entries, results, 0.2)
-        print(gd.errorFunction(linearGD, tethas, entries, results, entries.shape[0]))
-    print("final tethas -->>> ")
-    print(tethas)
-    plotLinear(entries, results, tethas)
+        thetaX = gd.runEpoch(linearGD, thetaX, time, dataX, 0.2)
+        # print("Error X -> {}".format(gd.errorFunction(linearGD, thetaX, time, dataX, time.shape[0])))
+        thetaY = gd.runEpoch(linearGD, thetaY, time, dataY, 0.2)
+        # print("Error Y -> {}".format(gd.errorFunction(linearGD, thetaY, time, dataY, time.shape[0])))
+        thetaZ = gd.runEpoch(linearGD, thetaZ, time, dataZ, 0.2)
+        print("Error Z -> {}".format(gd.errorFunction(linearGD, thetaZ, time, dataZ, time.shape[0])))
+        # print(" ----------------------------------------- ")
+
+    plotLinear(coordenatesArray, thetaX, thetaY, thetaZ)
 
 
 if __name__ == '__main__':
-    data = np.genfromtxt('kick1.dat')
-    runLinear(data[:, :2], data[:, 2])
-    runPolinomial(data[:, :2], data[:, 2])
+    data = np.genfromtxt('kick2.dat')
+    runLinear(data, np.linspace(1/60, 1/3, 20))
+    # runPolinomial(data[:, :2], data[:, 2])
